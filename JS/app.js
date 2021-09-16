c4496537a38a201d58967af4088da700bc6317b7
mahi1212 <mahi121.mr@gmail.com>
2021-09-16 19:56:45 +0600

    hover effect done

--- JS/app.js
+++ JS/app.js
@@ -2,8 +2,12 @@ const navbar = document.getElementById('nav')
 window.onscroll = () =>{
     if(window.pageYOffset > 300){
         navbar.classList.add('scrolled')
-        
+
     }else{
         navbar.classList.remove('scrolled')
     }
 }
+
+document.querySelector('.main-div').addEventListener('click', () => {
+    document.querySelector('.text-div').style.display = 'block'
+})
